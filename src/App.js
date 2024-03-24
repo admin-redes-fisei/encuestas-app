4dfb8dbd1853f68e73b8af54fd6f405f8d12f301
ArmijosThais <tarmijos02@gmail.com>
2024-03-24 18:50:32 -0500

    finalizado

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
-import Bachilleres from "./views/Bachilleres";
-import Empleabilidad from "./views/Empleabilidad";
 import EndPage from "./views/EndScreen";
 import StartPage from "./views/StartScreen";
 import FormularioPublico from "./views/FormularioPublico";
@@ -11,13 +9,8 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/encuestas/:for_alias" element={<StartPage />} />
-          <Route path="/encuestas/empleabilida" element={<Empleabilidad />} />
-          <Route
-            path="/encuestas/demanda-estudiante"
-            element={<Bachilleres />}
-          />
           <Route path="/encuestas/endpage" element={<EndPage />} />
+          <Route path="/encuestas/:for_alias" element={<StartPage />} />
           <Route
             path="/encuestas/:for_alias/:seccion"
             element={<FormularioPublico />}
