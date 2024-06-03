83b2ff44a66f6dedd4a2a2b7d26382b79f3982f0
ArmijosThais <tarmijos02@gmail.com>
2024-06-02 23:15:33 -0500

    Update PythonService.js

--- src/services/PythonService.js
+++ src/services/PythonService.js
@@ -1,5 +1,5 @@
-//const URL_acceso = "http://127.0.0.1:5000/";
-const URL_acceso = "http://localhost:5000/";
+const URL_acceso = "http://172.17.0.1:5000/";
+//const URL_acceso = "http://hatunsoft.uta.edu.ec:5000/";
 
 export async function enviarReglas(reglas) {
   try {
