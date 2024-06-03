0c3c40028fa2f85c0d8500e955d4b1f185e485fc
ArmijosThais <tarmijos02@gmail.com>
2024-06-02 20:21:02 -0500

    actualizacion servicio

--- src/services/PythonService.js
+++ src/services/PythonService.js
@@ -1,5 +1,5 @@
-const URL_acceso = "http://127.0.0.1:5000/";
-//const URL_acceso = "https://hatunsoft.uta.edu.ec/back_encuestas/";
+//const URL_acceso = "http://127.0.0.1:5000/";
+const URL_acceso = "http://hatunsoft.uta.edu.ec:5000/";
 
 export async function enviarReglas(reglas) {
   try {
